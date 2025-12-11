b0db2ea7cfa2b06a1f87f646042658466d149bd7
Meg Sheppard <142318637+megshep@users.noreply.github.com>
2025-12-11 11:20:31 +0000

    Update geodesic_shooting.sh
    removing job array function and increasing time limit

--- geodesic_shooting.sh
+++ geodesic_shooting.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=shoot_template
-#SBATCH --output=/mnt/iusers01/nm01/j90161ms/logs/shoot_logs/shoot_%A_%a.out
-#SBATCH --error=/mnt/iusers01/nm01/j90161ms/logs/shoot_logs/shoot_%A_%a.err
-#SBATCH --array=1-1229%1229        # Adjust as needed for number of subjects
-#SBATCH --time=72:00:00
-#SBATCH --mem=20G
-#SBATCH --cpus-per-task=1
+#SBATCH --output=/mnt/iusers01/nm01/j90161ms/logs/shoot_logs/shoot_%j.out
+#SBATCH --error=/mnt/iusers01/nm01/j90161ms/logs/shoot_logs/shoot_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=512G
+#SBATCH --time=4-00:00:00
+#SBATCH --partition=himem       # Use the high-memory partition
 
 # Load MATLAB module
 module load apps/binapps/matlab/R2024b
@@ -16,8 +18,9 @@ export USER_SPM_DIR="/mnt/iusers01/nm01/j90161ms/scratch/spm25/spm"
 # Change to the directory where you submitted the job
 cd "$SLURM_SUBMIT_DIR"
 
-# Run MATLAB non-interactively, calling your shoot.m script
+# Run MATLAB non-interactively, calling the geodesic shooting script
 matlab -nodisplay -nosplash -batch "try; addpath(getenv('USER_SPM_DIR')); run('/mnt/iusers01/nm01/j90161ms/trajectory/geodesic_shooting.m'); catch ME; disp(getReport(ME)); exit(1); end; exit(0);"
+
 
 
 
